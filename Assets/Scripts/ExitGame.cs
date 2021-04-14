40f8cb2a898b50ddf95f849c006973aa3c1a775a
Sven <Svenn.knappmann@gmail.com>
2021-04-14 10:11:14 +0200

    V0.6

---
+++ Assets/Scripts/ExitGame.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitGame : MonoBehaviour
+{
+    public AudioSource click;
+    public void Exit()
+    {
+        Application.Quit();
+        click.Play();
+    }
+}
